1b3658a2d4b6d55ff9664edcd0dd8685a0ac52dc
anemonemon <monika.skink@gmail.com>
2024-09-24 18:25:31 +0300

    Naujas

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print('hello')
-print('new')
+print('iveskite zodi')
+word = input()
+
+if len(word) < 5:
+    print(f'zodis {word} yra trumpas')
+elif len(word) < 10:
+    print(f'zodis {word} yra vidutionio ilgio')
+else:
+    print(f'zodis {word} yra ilgas')
+
